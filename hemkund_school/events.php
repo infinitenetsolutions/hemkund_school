213059f661abe27138d21f5f85edde455ae33ee7
Your Name <you@example.com>
2022-08-30 13:16:34 +0530

    updating 30

--- hemkund_school/events.php
+++ hemkund_school/events.php
@@ -123,7 +123,7 @@ foreach($results as $result)
                                             <td><?php echo htmlentities($result->msg);?></td>
                                             <td><?php echo htmlentities($result->description);?></td>
                                             <td><img src="img/vehicleimages/<?php echo htmlentities($result->image);?>" width="100" height="80" style="border:solid 1px #000"></td>
-											<td><a href="update_events.php?id=<?php echo $result->id;?>"><i class="fa fa-edit"></i></a>&nbsp;&nbsp;
+											<td><a href="update_event.php?id=<?php echo $result->id;?>"><i class="fa fa-edit"></i></a>&nbsp;&nbsp;
 												<a href="events.php?id=<?php echo $result->id;?>" onclick="return confirm('Do you want to delete');"><i class="fa fa-close"></i></a></td>
 										</tr>
 										<?php $cnt=$cnt+1; }} ?>
